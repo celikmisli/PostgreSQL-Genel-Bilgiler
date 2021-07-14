803b3c16404adab1e852981a939b9a554c1f424f
Misli Çelik <celikmisli@gmail.com>
2021-07-14 17:31:58 +0300

    Create Views.sql

---
+++ Views.sql
@@ -0,0 +1,25 @@
+/* POSTGRESQL VİEWS
+İstenen SQL tablolarını tıpkı gerçek bir tabloymuş gibi sanal bir şekilde toplayan bir özelliktir. 
+Uzun birleştirmelerde karmaşık sorguları oldukça basit bir hale getirirler.
+Direkt views sekmesinden işlemlerin yapıldığı gibi kodlarlada bu işlemler uygulanabilir.
+*/
+
+CREATE VIEW viewMisli
+AS
+SELECT c.country_name, r.region_name From countries c
+INNER JOIN regions r ON r.region_id=c.region_id
+
+-- Bu işlemleri oluşturduğunuz VİEWS'e sağ tıkladıktan sonra Proporties kısmından görebilirsiniz.
+
+SELECT * FROM viewMisli
+
+-- Ve artık yaptığımız işlemleri tek bir görüntü altında toplayabiliyoruz. :)
+
+CREATE VIEW viewMisli2
+AS
+INSERT INTO viewMisli2(id,name,surname) 
+VALUES (1, 'Misli', 'ÇELİK')
+
+/* VİEW WİTH CHECK OPTİON */
+--VİEW WİTH CHECK OPTİON yapısı insert, update, delete işlemlerinde where koşulu sağlanmıyorsa
+--bu işlemlerin gerçekleşmesini önler
